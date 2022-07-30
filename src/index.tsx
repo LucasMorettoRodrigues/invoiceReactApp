dec1142e4c43bdb1285c57e0190c4e97ccb769e0
Lucas <r.lucasmoretto@gmail.com>
2022-07-30 15:23:28 -0300

    create app/index.css, edit index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { App } from './App';
 
-const HelloWorld = () => {
-    return (
-        <h1>
-            Hello World
-        </h1>
-    );
-}
-
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
