4f5f38b0357c90d044c978a290034e308ac7f04c
Lucas <r.lucasmoretto@gmail.com>
2022-07-30 17:09:14 -0300

    Update ItemsTable and Create Table Components

--- src/components/invoice/itemsTable/ItemsTable.tsx
+++ src/components/invoice/itemsTable/ItemsTable.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { ChangeEvent } from "react"
+import { TInvoice } from "../../../types/Invoice"
+import { calculateGrandTotal, calculateTax, invoiceSubTotal } from "../../../utils/Functions"
+import { AddItem } from "./rows/AddItem"
+import { GrandTotal } from "./rows/GrandTotal"
+import { Header } from "./rows/Header"
+import { Item } from "./rows/Item"
+import { SubTotal } from "./rows/SubTotal"
+import { Tax } from "./rows/Tax"
 
-export const ItemsTable = () => {
+type Props = {
+    invoice: TInvoice,
+    handleChangeInvoice: (e: ChangeEvent<HTMLInputElement>) => void,
+    handleChangeItem: (e: ChangeEvent<HTMLInputElement>, index: number) => void,
+    handleAddItem: () => void,
+    handleRemoveItem: (index: number) => void
+    printMode: boolean,
+    currencySymbol: string
+}
+
+export const ItemsTable = ({ invoice, handleChangeInvoice, handleChangeItem, handleAddItem, handleRemoveItem, printMode, currencySymbol }: Props) => {
+
     return (
-        <div>ItemsTable</div>
+        <>
+            <Header
+            />
+            {invoice.items.map((item, index) => (
+                <Item
+
+                />
+            ))}
+            {
+                !printMode &&
+                <AddItem
+
+                />
+            }
+            <SubTotal
+
+            />
+            <Tax
+
+            />
+            <GrandTotal
+
+            />
+        </>
     )
 }
