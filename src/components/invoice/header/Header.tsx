9ad799af9663a007e5a4e9336bbb928d3a2d0d72
Lucas <r.lucasmoretto@gmail.com>
2022-07-30 15:56:57 -0300

    update Header

--- src/components/invoice/header/Header.tsx
+++ src/components/invoice/header/Header.tsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import styled from "styled-components"
+
+const SHeader = styled.div`
+  background-color: #357EBD;
+  color: #FFF;
+  padding: 0 14px;
+  margin-bottom: 1em;
+  text-align: center;
+  line-height: 2.5em;
+`
 
 export const Header = () => {
     return (
-        <div>Header</div>
+        <SHeader>
+            INVOICE
+        </SHeader>
     )
 }
