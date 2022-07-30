449069b32062500c65cc43dd277c4f6329ce8559
Lucas <r.lucasmoretto@gmail.com>
2022-07-30 15:44:49 -0300

    create Functions

---
+++ src/utils/Functions.ts
@@ -0,0 +1,25 @@
+import { TInvoice } from '../types/Invoice'
+
+// Calculates the sub total of the invoice
+export const invoiceSubTotal = (invoice: TInvoice) => {
+    let total = 0.00;
+    invoice.items.forEach(item => {
+        total += (item.qty * item.cost);
+    });
+    return total;
+};
+
+// Calculates the tax of the invoice
+export const calculateTax = (invoice: TInvoice) => {
+    return ((invoice.tax * invoiceSubTotal(invoice)) / 100);
+};
+
+// Calculates the grand total of the invoice
+export const calculateGrandTotal = (invoice: TInvoice) => {
+    return calculateTax(invoice) + invoiceSubTotal(invoice);
+};
+
+// Open print dialog
+export const printInfo = () => {
+    window.print();
+};
