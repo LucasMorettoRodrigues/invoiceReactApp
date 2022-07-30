f0e09d2fe95dc1e09a837b44132068d5bedd1202
Lucas <r.lucasmoretto@gmail.com>
2022-07-30 15:41:49 -0300

    create CurrencyService

---
+++ src/services/CurrencyService.ts
@@ -0,0 +1,30 @@
+export class CurrencyService {
+    all() {
+        return [
+            {
+                name: 'British Pound (£)',
+                symbol: '£'
+            },
+            {
+                name: 'Canadian Dollar ($)',
+                symbol: 'CAD $ '
+            },
+            {
+                name: 'Euro (€)',
+                symbol: '€'
+            },
+            {
+                name: 'Indian Rupee (₹)',
+                symbol: '₹'
+            },
+            {
+                name: 'Norwegian krone (kr)',
+                symbol: 'kr '
+            },
+            {
+                name: 'US Dollar ($)',
+                symbol: '$'
+            }
+        ]
+    }
+}
